4f012fb119bc4ccdaadb912e76ac5fc8f23c3258
iambasilp <basilxg321@gmail.com>
2023-02-18 15:43:32 +0530

    API Setup and Context  API

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AppContext } from "./context/contextApi";
+import { Header, Feed, SearchResult, VideoDetails } from "./components";
 const App = () => {
   return (
     <AppContext>
-      <div>App</div>
+      <BrowserRouter>
+        <div className="flex flex-col h-full">
+          <Header />
+          <Routes>
+            <Route path="/" exact element={<Feed/>} />
+            <Route path="/searchResults/:searchQuery" element={<SearchResult/>} />
+            <Route path="/video/:id" element={<VideoDetails/>} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </AppContext>
   );
 };
---
+++ src/components/index.js
@@ -0,0 +1,21 @@
+import Feed from "./Feed";
+import Header from "./Header";
+import LeftNav from "./LeftNav";
+import LeftNavMenuItem from "./LeftNavMenuItem";
+import SearchResult from "./SearchResult";
+import SearchResultVideo from "./SearchResultVideo";
+import SuggestionvideoCard from "./SuggestionvideoCard";
+import VideoDetails from "./VideoDetails";
+import VideoCard from "./VideoCard";
+
+export {
+    Feed,
+    Header,
+    LeftNav,
+    LeftNavMenuItem,
+    SearchResult,
+    SearchResultVideo,
+    SuggestionvideoCard,
+    VideoDetails,
+    VideoCard
+}
